1916a8393f03225da542217579f03b806abc4503
jiangxiaosheng <sj3129@columbia.edu>
2021-11-04 08:55:03 +0800

    completed feature: user sign-up/sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "goods#index"
   resources :goods
   get '/users/:username/profile', to: 'users#profile', as: :user_profile
-  match "user/:username/edit_profile" => "user#edit_profile", as: :edit_user_profile, via: [:get, :post]
+  match "/users/:username/edit_profile" => "users#edit_profile", as: :edit_user_profile, via: [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
