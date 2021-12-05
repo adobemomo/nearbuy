3eb79b5089b5b5a04cf2ad8a0cd07206031105a9
may.sincerity <may.sincerity@outlook.com>
2021-12-05 17:09:37 -0500

    modify explore mode

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -17,7 +17,11 @@ class GoodsController < ApplicationController
 
     @goods = Goods.all_goods.order(sort)
 
-    @goods = Goods.explore_goods(latitude, longitude, sort) if !latitude.nil? && !longitude.nil?
+    goods_map = nil
+    if !latitude.nil? && !longitude.nil?
+      goods_map = Goods.explore_goods(latitude, longitude, sort)
+      @goods = goods_map['goods']
+    end
 
     puts '----------------------------------------'
     @goods.each do |good|
@@ -25,11 +29,40 @@ class GoodsController < ApplicationController
     end
     puts '----------------------------------------'
 
-    @hash = Gmaps4rails.build_markers(@goods) do |good, marker|
-      marker.lat good.latitude
-      marker.lng good.longitude
-      marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
-    end
+    # @hash = Gmaps4rails.build_markers(@goods) do |good, marker|
+    #   marker.lat good.latitude
+    #   marker.lng good.longitude
+    #   marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
+    # end
+
+    @hash = if !latitude.nil? && !longitude.nil?
+              Gmaps4rails.build_markers(@goods) do |good, marker|
+                goods_map[good]['lats'].length.each do |i|
+                  marker.lat goods_map[good]['lats'][i]
+                  marker.lng goods_map[good]['longs'][i]
+                  marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
+                end
+              end
+            else
+              Gmaps4rails.build_markers(@goods) do |good, marker|
+                if !good.latitude.nil? && !good.longitude.nil?
+                  marker.lat good.latitude
+                  marker.lng good.longitude
+                  marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
+                end
+                if !good.latitude1.nil? && !good.longitude1.nil?
+                  marker.lat good.latitude1
+                  marker.lng good.longitude1
+                  marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
+                end
+                if !good.latitude2.nil? && !good.longitude2.nil?
+                  marker.lat good.latitude2
+                  marker.lng good.longitude2
+                  marker.infowindow render_to_string(partial: "/goods/map_box", locals: { good: good })
+                end
+              end
+            end
+
   end
 
   def show; end
--- app/models/goods.rb
+++ app/models/goods.rb
@@ -8,25 +8,55 @@ class Goods < ActiveRecord::Base
 
   def self.explore_goods(lat, long, sort)
     goods = Goods.where.not(name: nil, latitude: nil, longitude: nil).order(sort)
-    explore = []
+    explore = {
+      'goods': []
+    }
     lat = lat.to_f
     long = long.to_f
 
-    puts 'lat', lat
-    puts 'longititude', long
-
     goods.each do |good|
-      lat_diff = (lat - good.latitude) * 3.1415926 / 180.0
-      lng_diff = (long - good.longitude) * 3.1415926 / 180.0
-      lat_sin = Math.sin(lat_diff / 2.0)**2
-      lng_sin = Math.sin(lng_diff / 2.0)**2
-      first = Math.sqrt(lat_sin + Math.cos(lat * 3.1415926 / 180.0) * Math.cos(good.latitude * 3.1415926 / 180.0) * lng_sin)
-      result = Math.asin(first) * 2 * 6378137.0
-      puts result.to_i
-      explore.append(good) if result.to_i < 1000
+
+      lats = []
+      longs = []
+      if !good.latitude.nil? && !good.longitude.nil?
+        lats.append(good.latitude)
+        longs.append(good.longitude)
+      end
+      if !good.latitude1.nil? && !good.longitude1.nil?
+        lats.append(good.latitude1)
+        longs.append(good.longitude1)
+      end
+      if !good.latitude2.nil? && !good.longitude2.nil?
+        lats.append(good.latitude2)
+        longs.append(good.longitude2)
+      end
+
+      lats.length.each do |i|
+        lat_diff = (lat - lats[i]) * 3.1415926 / 180.0
+        lng_diff = (long - longs[i]) * 3.1415926 / 180.0
+        lat_sin = Math.sin(lat_diff / 2.0) ** 2
+        lng_sin = Math.sin(lng_diff / 2.0) ** 2
+        first = Math.sqrt(lat_sin + Math.cos(lat * 3.1415926 / 180.0) * Math.cos(lats[i] * 3.1415926 / 180.0) * lng_sin)
+        result = Math.asin(first) * 2 * 6378137.0
+        puts result.to_i
+        next unless result.to_i < 1000
+
+        unless explore.key?(good)
+          explore[good] = {
+            'lats': [],
+            'longs': []
+          }
+        end
+
+        explore[good]['lats'].append(lats[i])
+        explore[good]['longs'].append(longs[i])
+
+
+        # explore.append(good) if result.to_i < 1000
+      end
     end
 
     explore
   end
-
 end
+
